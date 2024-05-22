dfbb0526577f3c65cdaa2ecfff3da7d4d807c41b
ClaudiaDeTata <detatacl@gmail.com>
2024-05-22 12:52:26 +0100

    RPS Test Updated

--- testing/rps-js/cypress/cypress/e2e/rpsTest.cy.js
+++ testing/rps-js/cypress/cypress/e2e/rpsTest.cy.js
@@ -1,27 +1,96 @@
-describe('Rps Test', () => {
+describe('RPS Game', () => {
+  // open the url before each test
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:3000/c:/xampp/htdocs/Codespace2024-EC/testing/rps-js/rps.html'); 
-  });
-  
-  // testing buttons and checking their content
-  it('contains three buttons: Rock, Paper, Scissors', () => {
-    cy.get('.choices button').should('have.length', 3);
-    cy.get('#rock').should('exist').and('have.text', 'Rock');
-    cy.get('#paper').should('exist').and('have.text', 'Paper');
-    cy.get('#scissors').should('exist').and('have.text', 'Scissors');
-});
+    cy.visit('http://127.0.0.1:5500/rps.html')
+    // assign alias to the option button
+    cy.get('[data-cy="rock"]').as('rock')
+    cy.get('[data-cy="paper"]').as('paper')
+    cy.get('[data-cy="scissors"]').as('scissors')
+    // assign alias to the user-option, computer-option and result elements
+    cy.get('[data-cy="user-option"]').as('userOption')
+    cy.get('[data-cy="computer-option"]').as('computerOption')
+    cy.get('[data-cy="result"]').as('result')
+  })
+ 
+  // play the game with 'Rock' option and checks the result
+  it('Play the game with the Rock option and checks the result', () => {
+    // click on the 'Rock' button should exist
+    cy.get('@rock').should('exist').click()
 
-// simulating the buttons are clicked and checking their text content
-it('updates the user choice when a button is clicked', () => {
-  cy.get('#rock').click();
-  cy.get('#user-choice').should('have.text', 'rock');
+    // check that the user option is set to 'Rock'
+    cy.get('@userOption').should('have.text', "Rock")
 
-  cy.get('#paper').click();
-  cy.get('#user-choice').should('have.text', 'paper');
+    // check the computer option
+    cy.get('@computerOption').then((option) => {
+      // if the computer option is set to 'Rock'
+      if (option.text().includes("Rock")) {
+        // the result is a tie
+        cy.get('@result').contains("it's a tie!")
+      } 
+      // if the computer option is set to 'Paper'
+      else if (option.text().includes("Paper")) {
+        // the result is a computer win
+       cy.get('@result').contains("you lose!")
+      // if the computer option is set to 'Scissors'
+      } else {
+          // the result is a user win
+          cy.get('@result').contains("you win!")
+      }
+    })
+  })
 
-  cy.get('#scissors').click();
-  cy.get('#user-choice').should('have.text', 'scissors');
-});
+  // play the game with 'Paper' option and checks the result
+  it('Play the game with the Paper option and checks the result', () => {
+    // click on the 'Paper' button should exist
+    cy.get('@paper').should('exist').click()
 
+    // check that the user option is set to 'Paper'
+    cy.get('@userOption').should('have.text', "Paper")
 
-});
+    // check the computer option
+    cy.get('@computerOption').then((option) => {
+      // if the computer option is set to 'Paper'
+      if (option.text().includes("Paper")) {
+        // the result is a tie
+        cy.get('@result').contains("it's a tie!")
+      } 
+      // if the computer option is set to 'Rock'
+      else if (option.text().includes("Rock")) {
+        // the result is a user win
+       cy.get('@result').contains("you win!") 
+     }
+      // if the computer option is set to 'Scissors'
+      else {
+          // the result is a computer win
+        cy.get('@result').contains("you lose!")
+      }
+    })
+  })
+
+  // play the game with 'Scissors' option and check the result
+   it('Play the game with the Scissors option and checks the result', () => {
+    // click on the 'Scissors' button should exist
+    cy.get('@scissors').should('exist').click()
+
+    // check that the user option is set to 'Scissors'
+    cy.get('@userOption').should('have.text', "Scissors")
+
+    // check the computer option
+    cy.get('@computerOption').then((option) => {
+      // if the computer option is set to 'Scissors'
+      if (option.text().includes("Scissors")) {
+        // the result is a tie
+        cy.get('@result').contains("it's a tie!")
+      } 
+      // if the computer option is set to 'Rock'
+      else if (option.text().includes("Rock")) {
+        // the result is a computer win
+       cy.get('@result').contains("you lose!") }
+         // if the computer option is set to 'Paper'
+         else {
+          // the result is a user win
+          cy.get('@result').contains("you win!")
+      }
+    })
+  })
+})
